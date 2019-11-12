56634561d92268a66839d3a856ad53d931abbf9b
eclisia <florent.tainturier@gmail.com>
2019-11-12 16:15:43 +0100

    Update of timestamp

--- Code_Python/final-time-lapse.py
+++ Code_Python/final-time-lapse.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2.7
+#-*- coding: utf-8 -*-
 from time import sleep
 import picamera
 
@@ -5,5 +7,5 @@ WAIT_TIME = 10
 
 with picamera.PiCamera() as camera:
     camera.resolution = (2592, 1944)
-    for filename in camera.capture_continuous('/home/shares/public/img{timestamp:%H-%M-%S-%f}.jpg'):
+    for filename in camera.capture_continuous('/home/shares/public/img{timestamp:%Y%m%d-%H%M%S-%f}.jpg'):
         sleep(WAIT_TIME)
